61d84f3d113167da195f430b1c31fe14e32754ed
ghtjrtksxk <44721599+ghtjrtksxk@users.noreply.github.com>
2025-06-10 21:30:46 +0900

    feat : 씬 연결 & 메인 씬 스크립트 추가

--- Assets/Scripts/CreateUIManager.cs
+++ Assets/Scripts/CreateUIManager.cs
@@ -26,7 +26,7 @@ public class CreateUIManager : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene("SampleScene");
+            SceneManager.LoadScene("MainScene");
         }
     }
 
